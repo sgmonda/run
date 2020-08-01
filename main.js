166210a4e5dc691158be3874890403c545e2e8b1
Sergio Garcia Mondaray <sgmonda@gmail.com>
2020-08-01 12:08:43 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,38 @@
+input = document.getElementById("input");
+output = document.getElementById("output");
+
+const add = something => {
+  originalLog('PRINT', something);
+  output.innerHTML = output.innerHTML + something;
+}
+const originalError = console.error;
+const originalLog = console.log;
+const originalWarning = console.warn;
+const originalInfo = console.info;
+const originalClear = console.clear;
+
+console.error = function(error) {
+  add(error.toString() + error.stack);
+  originalError.apply(console, arguments);
+};
+console.log = function(...args) {
+  args.forEach(add);
+  originalLog.apply(console, args);
+};
+console.warn = function(...args) {
+  args.forEach(add);
+  originalWarning.apply(console, args);
+};
+console.info = function(...args) {
+  args.forEach(add);
+  originalInfo.apply(console, args);
+};
+console.clear = function(...args) {
+  element.innerHTML = '';
+  originalClear.apply(console, args);
+};
+
+document.getElementById("button-run").onclick = function run() {
+  output.innerHTML = '';
+  eval(input.innerText)
+}
